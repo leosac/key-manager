bebdbfa833398cb4b0d89bd6b06e32a7accc9059
Maxhy <maxime.chamley@leosac.com>
2023-03-21 22:43:05 +0100

    Disable key inputs when Key Link is setup

--- KeyManager.Library.UI/Domain/NullToBooleanConverter.cs
+++ KeyManager.Library.UI/Domain/NullToBooleanConverter.cs
@@ -10,19 +10,40 @@ namespace Leosac.KeyManager.Library.UI.Domain
 {
     public class NullToBooleanConverter : IValueConverter
     {
+        protected bool NullToBoolean(object parameter)
+        {
+            if (parameter != null)
+            {
+                if (parameter is bool v)
+                {
+                    return v;
+                }
+            }
+
+            return false;
+        }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var b = NullToBoolean(parameter);
             if (value == null)
-                return false;
+                return b;
 
-            return true;
+            if (value is string s)
+            {
+                if (string.IsNullOrEmpty(s))
+                    return b;
+            }
+
+            return !b;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var b = NullToBoolean(parameter);
             if (value is bool v)
             {
-                return (v == true) ? 0 : null;
+                return (v == !b) ? 0 : null;
             }
 
             return null;
