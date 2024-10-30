2ea6b4ce28b6f5c24453cc15dfa3d007702bb332
Maxime C. <maxime@leosac.com>
2024-10-30 02:33:59 +0100

    Fix KCV tests

--- KeyManager.Library.Tests/KCVTests.cs
+++ KeyManager.Library.Tests/KCVTests.cs
@@ -4,27 +4,27 @@
     public class KCVTests
     {
         [TestMethod]
-        public void Test_KCV_2K3DES()
+        public void Test_KCV_GlobalPlatform_2K3DES()
         {
-            var kcv = new KeyGen.KCV();
+            var kcv = new KeyGen.KCVGlobalPlatform();
             var checksum = kcv.ComputeKCV("DES", "11223344556677889900AABBCCDDEEFF");
 
             Assert.AreEqual("5ED7EA", checksum);
         }
 
         [TestMethod]
-        public void Test_KCV_3K3DES()
+        public void Test_KCV_GlobalPlatform_3K3DES()
         {
-            var kcv = new KeyGen.KCV();
+            var kcv = new KeyGen.KCVGlobalPlatform();
             var checksum = kcv.ComputeKCV("DES", "11223344556677889900AABBCCDDEEFF8877665544332211");
 
             Assert.AreEqual("CB799D", checksum);
         }
 
         [TestMethod]
-        public void Test_KCV_AES()
+        public void Test_KCV_GlobalPlatform_AES()
         {
-            var kcv = new KeyGen.KCV();
+            var kcv = new KeyGen.KCVGlobalPlatform();
             var checksum = kcv.ComputeKCV("AES", "11223344556677889900AABBCCDDEEFF");
 
             // Would be DD566B if not using 0x01 as padding value for IV
