480052fb03c2806a49d734d6bdb092d4280c7412
aurelm-w <aurelien.mascaro@leosac.com>
2026-02-17 15:06:37 +0100

    KeePass Plugin UI

---
+++ KeyManager.Library.KeyStore.KeePass.UI/KeePassStoreControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyManager.Library.KeyStore.KeePass.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class KeePassKeyStoreControl : UserControl
+    {
+        public KeePassKeyStoreControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            // file browser for .kdbx files
+        }
+
+        private void BtnRecent_Click(object sender, RoutedEventArgs e)
+        {
+            // recent db (optional, not sure to keep)
+        }
+
+        private void BtnKeyFile_Click(object sender, RoutedEventArgs e)
+        {
+            // key file browser dialog
+        }
+
+        private void BtnTest_Click(object sender, RoutedEventArgs e)
+        {
+            // connection test
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TxtFilePath.Clear();
+            Password.Clear();
+            TxtStatus.Text = "Ready to connect • No database selected";
+        }
+    }
+}
--- KeyManager.Library.KeyStore.KeePass/KeePassStore.cs
+++ KeyManager.Library.KeyStore.KeePass/KeePassStore.cs
@@ -1,43 +1,33 @@
 ﻿using KeePassLib;
 using KeePassLib.Keys;
 using KeePassLib.Security;
+using System.Collections.Generic;
+using KeePassLib.Serialization;
 
 namespace Leosac.KeyManager.Library.KeyStore.KeePass
 {
     public class KeePassStore : KeyStore
     {
         public override string Name => "KeePass";
+        private const string GroupName = "Credential";
+        private PwDatabase _database;
 
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType);
 
+        public override bool CanCreateKeyEntries => true;
+        public override bool CanDeleteKeyEntries => true;
 
+        public override IEnumerable<KeyEntryClass> SupportedClasses
+        {
+            get => new KeyEntryClass[] { KeyEntryClass.Symmetric, KeyEntryClass.Asymmetric };
+        }
 
-
-
-
-
-
-        public override bool CanCreateKeyEntries => throw new NotImplementedException();
-
-        public override bool CanDeleteKeyEntries => throw new NotImplementedException();
-
-        public override IEnumerable<KeyEntryClass> SupportedClasses => throw new NotImplementedException();
+        public override Task Open()
+        {
+            throw new NotImplementedException();
+        }
 
         public override Task<bool> CheckKeyEntryExists(KeyEntryId identifier, KeyEntryClass keClass)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Task Create(IChangeKeyEntry keyEntry)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Task Delete(KeyEntryId identifier, KeyEntryClass keClass, bool ignoreIfMissing)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Task<KeyEntry?> Get(KeyEntryId identifier, KeyEntryClass keClass)
         {
             throw new NotImplementedException();
         }
@@ -47,12 +37,22 @@ namespace Leosac.KeyManager.Library.KeyStore.KeePass
             throw new NotImplementedException();
         }
 
-        public override Task Open()
+        public override Task Create(IChangeKeyEntry keyEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<KeyEntry?> Get(KeyEntryId identifier, KeyEntryClass keClass)
         {
             throw new NotImplementedException();
         }
 
         public override Task Update(IChangeKeyEntry keyEntry, bool ignoreIfMissing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task Delete(KeyEntryId identifier, KeyEntryClass keClass, bool ignoreIfMissing)
         {
             throw new NotImplementedException();
         }
